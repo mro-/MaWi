9e71d468271f17f39314314b21291ab26d00b171
lro89 <lr@web-computing.de>
2015-06-17 11:56:15 +0200

    Mini Änderung Shared Variables

--- src/parallel/SharedVariables.java
+++ src/parallel/SharedVariables.java
@@ -90,11 +90,5 @@ public class SharedVariables {
 	 * Array, das die Berechnungsservices beinhaltet. (Aufteilung in Scheiben)
 	 */
 	public static ComputingAlgorithmus[] computingServices = new ComputingAlgorithmus[InitializeParameter.NUMBER_OF_THREADS];
-	
-	
-	/**
-	 * 
-	 */
-	public static float sinTemp = InitializeParameter.RTL;
 
 }
